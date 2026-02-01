2f907bd1727888f5fdb4500fa8907c7d15706a94
dav2010ID <dashkodavid2010@gmail.com>
2026-02-01 11:19:29 +0100

    add gtceu:uv_assembler

---
+++ test.lua
@@ -0,0 +1,14 @@
+local test = peripheral.wrap("top")
+local f = fs.open("slots.txt", "w")
+
+for i = 1, 11 do
+  local d = test.getItemDetail(i)
+
+  if d then
+    f.writeLine(i .. textutils.serialize(d))
+  else
+    f.writeLine(i .. " empty")
+  end
+    sleep(0)
+end
+f.close()
